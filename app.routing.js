8c2455931a07ce46a789f6c6479ad314d82369e5
A. Vatsaev <azero79@gmail.com>
2016-10-17 11:56:32 +0200

    removed clutter

--- app.routing.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.AppRoutes = [
-    { path: "", redirectTo: "home", pathMatch: "full" },
-    { path: '**', redirectTo: 'home' }
-];
-//# sourceMappingURL=app.routing.js.map
--- main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var platform_1 = require("nativescript-angular/platform");
-var app_module_1 = require("./app.module");
-platform_1.platformNativeScriptDynamic().bootstrapModule(app_module_1.AppModule);
-//# sourceMappingURL=main.js.map
